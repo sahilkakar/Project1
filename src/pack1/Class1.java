b58aa7b6b146048f839f9ee45321d2ab05bebd93
sahilkakar <sahil.kakar000@gmail.com>
2022-07-25 13:38:28 +0530

    New project (Project 1) 2nd commit...........

---
+++ src/pack1/Class1.java
@@ -0,0 +1,22 @@
+
+/*1. from quick access, search git repository
+2. git repositories small window on left corder will open
+3.Right click on project and select Teams  -> share project. 
+4. select  use/create repo in parent folder and click finish (proj will be added to the left bottom section)
+5. Right click again on project, select to index (now share will be added to the staging area)
+Again right click on project , teams -> commit 
+*/
+package pack1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		System.out.println("this is first java class for git hub");
+
+	}
+
+	
+	
+	
+	
+}
