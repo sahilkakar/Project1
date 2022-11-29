f6ff411d066542734a9a536af126715e10d94752
sahilkakar <sahil.kakar000@gmail.com>
2022-11-29 15:55:07 +0530

    added cool features

---
+++ src/pack1/Cool_Feature.java
@@ -0,0 +1,15 @@
+package pack1;
+
+public class Cool_Feature {
+    
+int a=1;
+
+public void m1(){
+
+
+System.out.println("this is test sample for VS code automation");
+System.out.println(a);
+
+}
+    
+}
